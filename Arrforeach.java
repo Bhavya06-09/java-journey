01a7f3676fabde18d24b71b102fea1165080671d
Bhavya06-09 <bbharadwaj238@gmail.com>
2026-03-21 23:56:44 +0530

    Add Switch, Break, continue, for each and elif

---
+++ Arrforeach.java
@@ -0,0 +1,11 @@
+public class Arrforeach 
+{
+    public static void main(String[] args) 
+    {
+        int[] numbers = {70,30,50,20,95};
+        for (int num : numbers) 
+        {
+            System.out.println(num);
+        }
+    }
+}
---
+++ Egswitch.java
@@ -0,0 +1,19 @@
+public class Egswitch 
+{
+    public static void main(String[] args) 
+{
+        int month = 2;
+        switch(month)
+{
+            case 1:
+                System.out.println("August");
+                break;
+            case 2:
+                System.out.println("September");
+                break;
+            case 3:
+                System.out.println("October");
+                break;
+        }
+    }
+}
